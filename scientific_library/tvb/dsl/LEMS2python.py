47c8a675cc4670792719d8c6460b0c203f061c5b
DeLaVlag <michielvandervlag@gmail.com>
2020-09-03 16:34:00 +0200

    vbb first work

--- scientific_library/tvb/dsl/LEMS2python.py
+++ scientific_library/tvb/dsl/LEMS2python.py
@@ -14,6 +14,15 @@ import os
 from tvb.basic.logger.builder import get_logger
 from tvb.dsl.NeuroML.lems.model.model import Model
 
+import csv
+import numpy as np
+
+from tvbtest_dataset.tvbtest_dataset import *
+
+from kgquery.queryApi import KGClient
+
+# from jupyter_collab_storage import oauth_token_handler as oauth
+
 logger = get_logger(__name__)
 
 def default_lems_folder():
@@ -52,9 +61,40 @@ def default_template():
     template = Template(filename=tmp_filename)
     return template
 
+def load_atlas_data():
+
+    atlas_data = []
+    atlas_dir=os.path.join(os.path.dirname(os.path.abspath(__file__)), 'NeuroML', 'atlas_data')
+    for findex, filename in enumerate(os.listdir(atlas_dir)):
+        if filename.__contains__("mha"):
+            file = (os.path.join(atlas_dir, filename))
+            atlas_data.insert(findex, np.genfromtxt(file, delimiter=','))
+
+    return atlas_data
+
+def process_atlas_data(atlas_data):
+
+    # for now normalize the data
+    for regions, data in enumerate(atlas_data):
+        datalength=(len(data))
+        for regs, dat in enumerate(data):
+            atlas_data[regions][regs]=atlas_data[regions][regs]/datalength
+
+    return atlas_data
+
+def annotate_template_data(atlas_data, model_name, model):
+
+    # for regions, data in enumerate(atlas_data):
+    cnstcntr=0
+    for cnstnr, cnst in enumerate(model.component_types[model_name].constants):
+        if 'mha' in cnst.name:
+            cnst.default=atlas_data[cnstcntr]
+            cnstcntr += 1
+
 
 def render_model(model_name, template=None, folder=None):
     model, svboundaries = load_model(model_name, folder)
+    annotate_template_data(process_atlas_data(load_atlas_data()), model_name, model)
     template = template or default_template()
     model_str = template.render(
                             dfunname=model_name,
@@ -90,7 +130,7 @@ def regTVB_templating(model_filename, folder=None):
     with open(modelfile, "w") as f:
         f.writelines(model_str)
 
-    # write new model to init.py such it is familiar to TVB if not already present
+    # write new model to __init__.py such it is familiar to TVB if not already present
     try:
         doprint=True
         modelenumnum=0
@@ -117,4 +157,17 @@ def regTVB_templating(model_filename, folder=None):
 if __name__ == "__main__":
 
     # example run for ReducedWongWang model
-    regTVB_templating('ReducedWongWangT', './NeuroML/XMLmodels/')
+    regTVB_templating('MontbrioT', './NeuroML/XMLmodels/')
+
+    # print(os.environ)
+    token = os.environ["HBP_AUTH_TOKEN"]="eyJhbGciOiJSUzI1NiIsImtpZCI6ImJicC1vaWRjIn0.eyJleHAiOjE1OTg4OTczNjcsInN1YiI6IjMwODEyNiIsImF1ZCI6WyJuZXh1cy1rZy1zZWFyY2giXSwiaXNzIjoiaHR0cHM6XC9cL3NlcnZpY2VzLmh1bWFuYnJhaW5wcm9qZWN0LmV1XC9vaWRjXC8iLCJqdGkiOiIxOGVmNmM4NC1kNjA5LTQxOGYtYTE1Zi1kNWY5OTQ2YzJkODYiLCJpYXQiOjE1OTg4ODI5NjcsImhicF9rZXkiOiIzYTcwNjdjNWZmYjZlZTY4YTVjNDUyMDU2MTI5NjI2Mzc1NjcyODA4In0.cAnkmp25ZIymoT2THhg7odlUbNnFMST3VZ4W-gw8-sbyrompVzEsf48pEXnWP1u-fKXmI1_EW3jTNYYkpkb9DzWeIwveIcw-s7W9FdEob3e27P4zmndRCyrq1E8ubRVCU1xQ4pPIZl1tSpT1UBnef8uXbIFMgyaXzrIBUJ8z1BY"
+    client = KGClient(token, "https://kg.humanbrainproject.eu/query")
+    # example = TVBtestDataset(client, '10.25493/1ECN-6SM')
+    example = TVBtestDataset(client)
+    # print(example.create_filter_params())
+    dataatje = example.fetch()
+
+    # datadict={}
+    # returneddatadict={}
+    # returneddatadict = example.create_result(datadict)
+    # print(vars(returneddatadict))
