269b11ac43bc68644d77260fd0133cd52573bb01
Aarón Pérez <a.perez.martin@fz-juelich.de>
2021-02-17 14:19:10 +0100

    error handle & rateml_test.py for testing

--- scientific_library/tvb/rateML/XML2model.py
+++ scientific_library/tvb/rateML/XML2model.py
@@ -33,6 +33,7 @@ LEMS2python module implements a DSL code generation using a TVB-specific LEMS-ba
 
 .. moduleauthor:: Michiel. A. van der Vlag <m.van.der.vlag@fz-juelich.de>   
 .. moduleauthor:: Marmaduke Woodman <marmaduke.woodman@univ-amu.fr>
+.. moduleauthor:: Aarón Pérez Martín <a.perez.martin@fz-juelich.de>
 
 
         Usage: - create an modelfile.xml
@@ -51,7 +52,7 @@ LEMS2python module implements a DSL code generation using a TVB-specific LEMS-ba
 
 """
 
-import os, sys
+import os, sys, glob
 import tvb.simulator.models
 from mako.template import Template
 import re
@@ -65,6 +66,31 @@ from lems.model.model import Model
 
 logger = get_logger(__name__)
 
+class Utils:
+    @staticmethod
+    def get_onlyname(fullpath):
+        basename = os.path.basename(fullpath)
+        filename, extension = os.path.splitext(basename)
+        return filename, extension
+
+    @staticmethod
+    def load_models(location, extension):
+        full_filenames, only_filenames = [], []
+        print("\nModels found:")
+        for file in glob.glob(os.path.join(location, "*")):
+            if (os.path.isfile(file)):
+                only_name, ext = Utils.get_onlyname(file)
+                if ext.lower() == str(extension):
+                    full_filenames.append(file)
+                    only_filenames.append(only_name)
+                    print(" - ", file)
+        return full_filenames, only_filenames
+
+    @staticmethod
+    def model_exist(keyword, list):
+        return keyword.lower() in [item.lower() for item in list]
+
+
 class RateML:
 
     def __init__(self, model_filename, language='python', XMLfolder=None, GENfolder=None):
@@ -77,15 +103,24 @@ class RateML:
         self.generated_model_location = self.set_generated_model_location()
         self.xml_location = self.set_XML_model_folder()
 
-        # start templating
-        model_str = self.render_model()
+    def transform(self):
 
-        # write model to user submitted location
-        self.write_model_file(self.generated_model_location, model_str)
+        try:
+            # start templating
+            model_str, validation = self.render_model()
+            if len(validation)>0:
+                return False, validation
 
-        # if it is a TVB.py model, it should be familiarized
-        if self.language.lower()=='python':
-            self.familiarize_TVB(model_str)
+            # write model to user submitted location
+            self.write_model_file(self.generated_model_location, model_str)
+
+            # if it is a TVB.py model, it should be familiarized.
+            # TODO it make the whole Server reset the session.
+            #if self.language.lower()=='python':
+            #    self.familiarize_TVB(model_str)
+            return True, ""
+        except Exception as e:
+            return False, str(e)
 
     @staticmethod
     def default_XML_folder():
@@ -95,7 +130,7 @@ class RateML:
 
     def set_XML_model_folder(self):
         folder = self.XMLfolder or self.default_XML_folder()
-        return os.path.join(folder, self.model_filename.lower() + '.xml')
+        return os.path.join(folder, self.model_filename + '.xml') #.lower()
 
     @staticmethod
     def default_generation_folder():
@@ -121,19 +156,24 @@ class RateML:
     def XSD_validate_XML(self):
 
         ''' Use own validation instead of LEMS because of slight difference in definition file'''
+        try:
+            from lxml import etree
+            from urllib.request import urlopen
 
-        from lxml import etree
-        from urllib.request import urlopen
+            # Local XSD file location
+            # schema_file = urlopen("file:///home/michiel/Documents/Repos/tvb-root/github/tvb-root/scientific_library/tvb/rateML/rML_v0.xsd")
 
-        # Local XSD file location
-        # schema_file = urlopen("file:///home/michiel/Documents/Repos/tvb-root/github/tvb-root/scientific_library/tvb/rateML/rML_v0.xsd")
+            # Global XSD file location
+            schema_file = urlopen(
+                "https://raw.githubusercontent.com/DeLaVlag/tvb-root/xsdvalidation/scientific_library/tvb/rateML/rML_v0.xsd")
+            xmlschema = etree.XMLSchema(etree.parse(schema_file))
+            xmlschema.assertValid(etree.parse(self.xml_location))
+            logger.info("True validation of {0} against {1}".format(self.xml_location, schema_file.geturl()))
+            return True, ""
 
-        # Global XSD file location
-        schema_file = urlopen(
-            "https://raw.githubusercontent.com/DeLaVlag/tvb-root/xsdvalidation/scientific_library/tvb/rateML/rML_v0.xsd")
-        xmlschema = etree.XMLSchema(etree.parse(schema_file))
-        xmlschema.assertValid(etree.parse(self.xml_location))
-        logger.info("True validation of {0} against {1}".format(self.xml_location, schema_file.geturl()))
+        except Exception as error:
+            return False, str(error)
+
 
     def powerswap(self, power):
         target = power.group(1)
@@ -146,89 +186,91 @@ class RateML:
     def preprocess_model(self, model):
 
         ''' Do some preprocessing on the template to easify rendering '''
+        try:
+            # check if boundaries for state variables are present. contruct is not necessary in pymodels
+            # python only
+            svboundaries = False
+            for i, sv in enumerate(model.component_types['derivatives'].dynamics.state_variables):
+                if sv.exposure != 'None' and sv.exposure != '' and sv.exposure:
+                    svboundaries = True
+                    continue
 
-        # check if boundaries for state variables are present. contruct is not necessary in pymodels
-        # python only
-        svboundaries = False
-        for i, sv in enumerate(model.component_types['derivatives'].dynamics.state_variables):
-            if sv.exposure != 'None' and sv.exposure != '' and sv.exposure:
-                svboundaries = True
-                continue
+            # check for component_types containing coupling in name and gather data.
+            # multiple coupling functions could be defined in xml
+            # cuda only
+            couplinglist = list()
+            for i, cplists in enumerate(model.component_types):
+                if 'coupling' in cplists.name:
+                    couplinglist.append(cplists)
 
-        # check for component_types containing coupling in name and gather data.
-        # multiple coupling functions could be defined in xml
-        # cuda only
-        couplinglist = list()
-        for i, cplists in enumerate(model.component_types):
-            if 'coupling' in cplists.name:
-                couplinglist.append(cplists)
+            # only check whether noise is there, if so then activate it
+            # cuda only
+            noisepresent=False
+            for ct in (model.component_types):
+                if ct.name == 'noise':
+                    noisepresent=True
 
-        # only check whether noise is there, if so then activate it
-        # cuda only
-        noisepresent=False
-        for ct in (model.component_types):
-            if ct.name == 'noise':
-                noisepresent=True
+            # see if nsig derived parameter is present for noise
+            # cuda only
+            modellist = model.component_types['derivatives']
+            nsigpresent=False
+            if noisepresent==True:
+                for dprm in (modellist.derived_parameters):
+                    if (dprm.name == 'nsig' or dprm.name == 'NSIG'):
+                         nsigpresent=True
 
-        # see if nsig derived parameter is present for noise
-        # cuda only
-        modellist = model.component_types['derivatives']
-        nsigpresent=False
-        if noisepresent==True:
-            for dprm in (modellist.derived_parameters):
-                if (dprm.name == 'nsig' or dprm.name == 'NSIG'):
-                     nsigpresent=True
+            # check for power symbol and parse to python (**) or c power (powf(x, y))
+            # there are 5 locations where they can occur: Derivedvariable.value, ConditionalDerivedVariable.Case.condition
+            # Derivedparameter.value, Time_Derivaties.value and Exposure.dimension
+            # Todo make more generic, XML tag processing might be the key
 
-        # check for power symbol and parse to python (**) or c power (powf(x, y))
-        # there are 5 locations where they can occur: Derivedvariable.value, ConditionalDerivedVariable.Case.condition
-        # Derivedparameter.value, Time_Derivaties.value and Exposure.dimension
-        # Todo make more generic, XML tag processing might be the key
-
-        for cptype in model.component_types:
-            powlst = model.component_types[cptype.name]
-            # list of locations with mathmatical expressions with attribute 'value'
-            power_parse_exprs_value = [powlst.derived_parameters, powlst.dynamics.derived_variables,
-                                       powlst.dynamics.time_derivatives]
-            for pwr_parse_object in power_parse_exprs_value:
-                for pwr_obj in pwr_parse_object:
-                    if '^' in  pwr_obj.value:
-                        if self.language=='python':
-                            if hasattr(pwr_obj, 'name'):
-                                pwr_parse_object[pwr_obj.name].value = pwr_obj.value\
-                                    .replace('{', '').replace('^', ' ** ').replace('}', '')
-                            if hasattr(pwr_obj, 'variable'):
-                                pwr_parse_object[pwr_obj.variable].value = pwr_obj.value\
-                                    .replace('{', '').replace('^', ' ** ').replace('}', '')
-                        if self.language=='cuda':
-                            for power in re.finditer('\{(.*?)\}',  pwr_obj.value):
-                                target, powf = self.powerswap(power)
+            for cptype in model.component_types:
+                powlst = model.component_types[cptype.name]
+                # list of locations with mathmatical expressions with attribute 'value'
+                power_parse_exprs_value = [powlst.derived_parameters, powlst.dynamics.derived_variables,
+                                           powlst.dynamics.time_derivatives]
+                for pwr_parse_object in power_parse_exprs_value:
+                    for pwr_obj in pwr_parse_object:
+                        if '^' in  pwr_obj.value:
+                            if self.language=='python':
                                 if hasattr(pwr_obj, 'name'):
-                                    pwr_parse_object[pwr_obj.name].value = pwr_obj.value.replace(target, powf)
+                                    pwr_parse_object[pwr_obj.name].value = pwr_obj.value\
+                                        .replace('{', '').replace('^', ' ** ').replace('}', '')
                                 if hasattr(pwr_obj, 'variable'):
-                                    pwr_parse_object[pwr_obj.variable].value = pwr_obj.value.replace(target, powf)
+                                    pwr_parse_object[pwr_obj.variable].value = pwr_obj.value\
+                                        .replace('{', '').replace('^', ' ** ').replace('}', '')
+                            if self.language=='cuda':
+                                for power in re.finditer('\{(.*?)\}',  pwr_obj.value):
+                                    target, powf = self.powerswap(power)
+                                    if hasattr(pwr_obj, 'name'):
+                                        pwr_parse_object[pwr_obj.name].value = pwr_obj.value.replace(target, powf)
+                                    if hasattr(pwr_obj, 'variable'):
+                                        pwr_parse_object[pwr_obj.variable].value = pwr_obj.value.replace(target, powf)
 
-            for pwr_obj in powlst.exposures:
-                if '^' in pwr_obj.dimension:
-                    if self.language=='python':
-                        powlst.exposures[pwr_obj.name].dimension = pwr_obj.dimension\
-                            .replace('{', '').replace('^', ' ** ').replace('}', '')
-                    if self.language=='cuda':
-                        for power in re.finditer('\{(.*?)\}', pwr_obj.dimension):
-                            target, powf = self.powerswap(power)
-                            powlst.exposures[pwr_obj.name].dimension = pwr_obj.dimension.replace(target, powf)
-
-            for cdv in powlst.dynamics.conditional_derived_variables:
-                for casenr, case in enumerate(cdv.cases):
-                    if '^' in case.value:
-                        if self.language == 'python':
-                            powlst.dynamics.conditional_derived_variables[cdv.name].cases[casenr].value = case.value\
+                for pwr_obj in powlst.exposures:
+                    if '^' in pwr_obj.dimension:
+                        if self.language=='python':
+                            powlst.exposures[pwr_obj.name].dimension = pwr_obj.dimension\
                                 .replace('{', '').replace('^', ' ** ').replace('}', '')
-                        if self.language == 'cuda':
-                            for power in re.finditer('\{(.*?)\}', case.value):
+                        if self.language=='cuda':
+                            for power in re.finditer('\{(.*?)\}', pwr_obj.dimension):
                                 target, powf = self.powerswap(power)
-                                powlst.dynamics.conditional_derived_variables[cdv.name].cases[casenr].value = case.value.replace(target, powf)
+                                powlst.exposures[pwr_obj.name].dimension = pwr_obj.dimension.replace(target, powf)
 
-        return svboundaries, couplinglist, noisepresent, nsigpresent
+                for cdv in powlst.dynamics.conditional_derived_variables:
+                    for casenr, case in enumerate(cdv.cases):
+                        if '^' in case.value:
+                            if self.language == 'python':
+                                powlst.dynamics.conditional_derived_variables[cdv.name].cases[casenr].value = case.value\
+                                    .replace('{', '').replace('^', ' ** ').replace('}', '')
+                            if self.language == 'cuda':
+                                for power in re.finditer('\{(.*?)\}', case.value):
+                                    target, powf = self.powerswap(power)
+                                    powlst.dynamics.conditional_derived_variables[cdv.name].cases[casenr].value = case.value.replace(target, powf)
+
+            return svboundaries, couplinglist, noisepresent, nsigpresent, ""
+        except Exception as error:
+            return None, None, None, None, str(error)
 
     def load_model(self):
         "Load model from filename"
@@ -240,9 +282,9 @@ class RateML:
         self.XSD_validate_XML()
 
         # do some inventory. check if boundaries are set for any sv to print the boundaries section in template
-        svboundaries, couplinglist, noisepresent, nsigpresent = self.preprocess_model(model)
+        svboundaries, couplinglist, noisepresent, nsigpresent, error = self.preprocess_model(model)
 
-        return model, svboundaries, couplinglist, noisepresent, nsigpresent
+        return model, svboundaries, couplinglist, noisepresent, nsigpresent, error
 
     def render_model(self):
         '''
@@ -250,16 +292,45 @@ class RateML:
         this function is similar for all languages. its .render arguments are overloaded.
         '''
 
-        model, svboundaries, couplinglist, noisepresent, nsigpresent = self.load_model()
-
-        derivative_list = model.component_types['derivatives']
 
         if self.language == 'python':
             model_class_name = self.model_filename.capitalize() + 'T'
         if self.language == 'cuda':
             model_class_name = self.model_filename
 
-        # start templating
+        validation = ""
+        derivative_list = None
+        model, svboundaries, couplinglist, noisepresent, nsigpresent, error = self.load_model()
+
+        #Checking model
+        if len(error)>0:
+            validation +=error
+        else:
+            derivative_list = model.component_types['derivatives']
+
+
+        #Checking minimum requirements
+        if derivative_list != None:
+
+            #Common Validation
+            if len(list(derivative_list.constants)) == 0:
+                validation += "Constant is missing.\n"
+            if len(list(derivative_list.exposures)) == 0:
+                validation += "Exposures is missing.\n"
+            if derivative_list.dynamics == None:
+                validation += "Dynamics is missing.\n"
+
+            #Python Validation
+
+            #Cuda Validation
+
+        else:
+            validation += "Derivatives component is missing.\n"
+
+        if len(validation)>0:
+            return None, validation
+
+            # start templating
         model_str = self.model_template().render(
             modelname=model_class_name,                     # all
             const=derivative_list.constants,                # all
@@ -273,7 +344,7 @@ class RateML:
             nsigpresent=nsigpresent,                        # cuda
             )
 
-        return model_str
+        return model_str, validation
 
     def familiarize_TVB(self, model_str):
         '''
