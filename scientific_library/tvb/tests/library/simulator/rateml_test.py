97aec937ec3321a43839c3c28abc2d1d42979b3c
Aarón Pérez <a.perez.martin@fz-juelich.de>
2021-03-01 18:44:40 +0100

    checking cuda model content

--- scientific_library/tvb/tests/library/simulator/rateml_test.py
+++ scientific_library/tvb/tests/library/simulator/rateml_test.py
@@ -1,16 +1,31 @@
-import pytest, os, glob, itertools, numpy as np
+import pytest, os, glob, itertools, numpy as np, re
+from tvb.rateML.run.__main__ import TVB_test
+from tvb.rateML.run.cuda_run import CudaRun
 from tvb.tests.library.base_testcase import BaseTestCase
 from tvb.rateML import XML2model
 from tvb.rateML.XML2model import RateML, Utils
+from pathlib import Path
 
 import pycuda.autoinit
 import pycuda.driver as cuda
 from pycuda.compiler import SourceModule
 
+from tvb.rateML.run import __main__
+from cuda_run import CudaRun
+
 
 framework_path, _ = os.path.split(XML2model.__file__)
 XMLModel_path = os.path.join(framework_path, "XMLmodels")
 generatedModels_path = os.path.join(framework_path, "generatedModels")
+#dic_regex_mincount = {r'^__global':1, r'^__device':1}
+dic_regex_mincount = {r'^__global':1,
+                      r'^__device':3,
+                      r'^__device__ float wrap_it_':2,
+                      r'state\(\(\(':1,
+                      r'state\(\(t':2,
+                      r'tavg\(':1,
+                      r'= params\(\d\)\;$':2}
+
 
 def compiler_opts():
     opts = ['--ptxas-options=-v', '-maxrregcount=32', '-lineinfo']
@@ -65,9 +80,63 @@ class TestRateML():
         compiled = False
         with open(source_file, 'r') as f:
             mod_content = mod_content.replace('M_PI_F', '%ff' % (np.pi,))
+
+            # Compile model
             mod = SourceModule(mod_content, options=compiler_opts(), include_dirs=[], no_extern_c=True, keep=False)
             assert mod is not None
             compiled = True
 
         assert compiled
 
+    @pytest.mark.slow
+    @pytest.mark.parametrize('model', models)
+    def test_simulation_cuda_models(self, model):
+        source_file = os.path.join(generatedModels_path, model + ".c")
+        with open(source_file, 'r') as f:
+            #mod_content = mod_content.replace('M_PI_F', '%ff' % (np.pi,))
+            #Compile model
+            #mod = SourceModule(mod_content, options=compiler_opts(), include_dirs=[], no_extern_c=True, keep=False)
+            #mod_func = "{}{}{}{}".format('_Z', len(model), model, 'jjjjjffPfS_S_S_S_')
+
+            #execute the function into the GPU
+            #func = mod.get_function(mod_func)
+            ###########
+
+            logging.basicConfig(level=logging.DEBUG if self.args.verbose else logging.INFO)
+            logger = logging.getLogger('[TVB_CUDA]')
+
+            cudarun = CudaRun()
+
+            tester = TVB_test()
+
+            tavg_data = cudarun.run_simulation(tester.weights, tester.lengths, tester.params, tester.speeds, logger,
+                                               tester.args, tester.n_nodes, tester.n_work_items, tester.n_params, tester.nstep,
+                                               tester.n_inner_steps, tester.buf_len, tester.states, tester.dt, tester.min_speed)
+            print(vars(tavg_data))
+
+    @pytest.mark.slow
+    @pytest.mark.parametrize('model', models)
+    def test_contentcheck_cuda_models(self, model):
+
+        source_file = os.path.join(generatedModels_path, model + ".c")
+        with open(source_file, "r") as f:
+            lines = f.read()
+
+            # trucate the file to avoid processing bold_update
+            lines = lines.split("__global__ void bold_update")[0]
+
+            pattern_model = r'^__global__ void ' + model + '\('
+            if len(re.findall(pattern=pattern_model, string=lines, flags=re.IGNORECASE + re.MULTILINE + re.DOTALL)) <= 0:
+                print("Error", pattern_model, "did not found", model)
+                assert False
+            else:
+                assert True
+
+            for regex, mincount in dic_regex_mincount.items():
+                matches = re.findall(pattern=regex, string=lines, flags=re.IGNORECASE + re.MULTILINE + re.DOTALL)
+
+                if len(matches) < dic_regex_mincount[regex]:
+                    print("Error", regex, "found", len(matches), "of", mincount)
+                    assert False
+                else:
+                    assert True
